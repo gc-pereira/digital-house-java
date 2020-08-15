43abefc1c4885e7f89b404ee93caf215cf75b63b
gc-pereira <carvalho.gabrielp@gmail.com>
2020-08-14 23:02:08 -0300

    criação de algo que simule uma empresa utilizando poo !

---
+++ desafio_aula03/PessoaJuridica.java
@@ -0,0 +1,15 @@
+package desafio_aula03;
+
+public class PessoaJuridica extends Funcionarios{
+	
+	private String cnpj;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+}
